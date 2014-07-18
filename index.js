98b2c379d4a91f1d065840494829995bd5748eab
Carlos Guerrero <guerrerocarlos@gmail.com>
2014-07-18 19:43:02 -0400

    learning

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ superSsdp.prototype.start = function () {
       };
       //console.log('search>>answer<<')
       //console.log(headers)
+      onReady()
       peer.reply(headers, address);
   }).on("found", function (headers, address) {
       //console.log('found>>')
