d4322c759dd0170eff8451130e39a0de31665ca9
Carlos Guerrero <guerrerocarlos@gmail.com>
2014-07-18 20:32:51 -0400

    learning

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ superSsdp.prototype.start = function () {
   var self = this
 
   var onReady = function () {
+      console.log('sending info request to the wild')
       peer.search({
           ST: "upnp:rootdevice"
       });
